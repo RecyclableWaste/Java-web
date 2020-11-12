5cacd7440b578f096365e97b2ef2dd50598e5ff5
RecyclableWaste <591890112@qq.com>
2020-11-12 21:21:45 +0800

    初步实现了IOC及MVC

--- ioc/src/main/java/cqu/ma/ioc/annotation/Component.java
+++
@@ -1,9 +0,0 @@
-package cqu.ma.ioc.annotation;
-
-import java.lang.annotation.*;
-
-@Target(ElementType.TYPE)
-@Retention(RetentionPolicy.RUNTIME)
-public @interface Component{
-    public String value() default "";
-}
